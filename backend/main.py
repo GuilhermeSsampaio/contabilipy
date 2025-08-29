60bfe913fb07ef6fc686ced20173742bfc601bc8
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-08-29 18:48:26 -0400

    init: criar estrutura inicial do projeto contabilipy

---
+++ backend/main.py
@@ -0,0 +1,16 @@
+import os
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+INTERFACE_URL = os.getenv("INTERFACE_URL", "http://localhost:8501")
+
+@app.get("/")
+def root():
+    return "Rota home"
+
+@app.get("/interface")
+def interface():
+        return RedirectResponse(INTERFACE_URL)
+
+
---
+++ interface/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write("""
+    Bem vindo ao *contabilipy*
+    Um sistema de programadores, para contadores!
+""")
